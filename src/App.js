b1001c3c3e036dd5565c8b1c8963b61fcc44d9b3
ijlalansari1122 <ansariijlal90@gmail.com>
2022-10-05 20:47:53 +0500

    using destructuring inplace with alias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const employee={
+
+id:'abc',
+name:'anito',
+salary:'$23',
+address:{
+salary: '$100',
+street: "sasas",
+country: "canada",
+province: "uinas"
+,
+zones:{
+
+plant:'4e',
+
+
+}
+}
 }
 
+
+ const App = () => {
+
+// using destructuring in place
+    const {name , id , salary,  address:{salary:addresssalary , street,country,province, zones:{plant}}} =employee;
+
+
+
+    return (
+        <div>
+<p>
+</p>
+
+<h1>Hello world</h1> 
+<h2>{name}, {id}, {salary} , </h2>
+<h3>{country} , {province } , {street} , {addresssalary}</h3>
+
+<h3>{plant}</h3>
+  </div>
+
+
+    )
+
+
+}
 export default App;
