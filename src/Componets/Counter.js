c22d02f522fac70ec714eafa69d02a1982b4bf10
ijlalansari1122 <ansariijlal90@gmail.com>
2022-10-29 13:58:55 +0500

    useeffect case

--- src/Componets/Counter.js
+++ src/Componets/Counter.js
@@ -4,34 +4,25 @@ import React, { useEffect, useState } from "react";
 const Counter =()=>{
 
     const [count, setcount] = useState(0);
-const[count2 , setcount2]= useState(0);
-    // component did mount
-
-    // useEffect(() => {
-    //     console.log("the use effect ran")
-
-
-
-    // }, []);
-    //     // componentwillupdate
 
     useEffect(() => {
-        console.log("the component has been updated")
+console.log("The component has mounted")       
 
-        return () => {
+return()=>{
 
-            console.log(`we are in the cleanup - the count is ${count} `)
 
-        }
+console.log("The component has unmounted")
 
-    }, [count]);
+}
+
+
+    },[]);
 
 return(
 <div>
 <h5>Counter</h5>
 <p>Current count: {count}</p>
 <button onClick={()=>setcount(count+1)}>Increment the count</button>
-<button onClick={() => setcount2(count2 + 1)}>Increment the count2</button>
 
 
 </div>
