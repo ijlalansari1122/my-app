d09e4881d5b279bf8acfd3b7b25b9cf069c65b68
ijlalansari1122 <ansariijlal90@gmail.com>
2022-10-29 14:16:57 +0500

    useeffect

--- src/Componets/Counter.js
+++ src/Componets/Counter.js
@@ -4,25 +4,34 @@ import React, { useEffect, useState } from "react";
 const Counter =()=>{
 
     const [count, setcount] = useState(0);
+    const [count1, setcount1] = useState(0);
 
-    useEffect(() => {
-console.log("The component has mounted")       
+    // useEffect(() => {
+    //     console.log("the use effect ran")
+    // }, [count , count1]);
 
-return()=>{
+// component did unmount
+useEffect(()=>{
 
+        console.log("the count has updated")
+// cleanup function
+        return()=>{
 
-console.log("The component has unmounted")
+console.log(`we are in the cleanup -the count is ${count} `);
 
 }
 
+}, [count])
 
-    },[]);
+
+
 
 return(
 <div>
 <h5>Counter</h5>
-<p>Current count: {count}</p>
-<button onClick={()=>setcount(count+1)}>Increment the count</button>
+<p>Current count: {count}   {count1} </p>
+        <button onClick={() => setcount(count + 1)}>Increment the count</button>
+        <button onClick={() => setcount1(count1 + 1)}>Increment the count2</button>
 
 
 </div>
